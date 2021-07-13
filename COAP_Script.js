575e73d6d0e716b4f806aa60df1d47d56a8a3a1f
Morteza-Damghani <mortezadamghani77@gmail.com>
2021-07-13 05:31:42 -0700

    This part is almost done

---
+++ COAP_Script.js
@@ -0,0 +1,9 @@
+function home_button_handler() {
+    document.getElementById("home_content").style.display = "block";
+
+}
+
+function farm_button_handler() {
+    document.getElementById("farm_content").style.display = "block";
+
+}
